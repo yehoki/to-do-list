3850186e11eb7ae09ce2b8d9b2803beb4e0e0834
yehoki <patsenn2000@gmail.com>
2023-04-09 19:57:39 +0100

    First DOM setup

---
+++ task.bundle.js
@@ -0,0 +1,39 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*********************!*\
+  !*** ./src/task.js ***!
+  \*********************/
+/*
+We will be using tasks in order to store, create and manipulate the 'todo' list objects.
+*/
+
+// Task factory
+
+const Task = (title, description, dueDate, priority, notes, location) => {
+  return {
+    title,
+    description,
+    dueDate,
+    priority,
+    notes,
+    location,
+  };
+};
+
+const taskController = (() => {
+  const tasks = [];
+  
+  const createTasks = () => {
+
+  }
+
+})
+
+
+//
+
+
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGFzay5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQTtBQUNBO0FBQ0E7O0FBRUE7O0FBRUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7O0FBRUE7QUFDQTtBQUNBO0FBQ0E7O0FBRUE7O0FBRUEsQ0FBQzs7O0FBR0QiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90by1kby1saXN0Ly4vc3JjL3Rhc2suanMiXSwic291cmNlc0NvbnRlbnQiOlsiLypcbldlIHdpbGwgYmUgdXNpbmcgdGFza3MgaW4gb3JkZXIgdG8gc3RvcmUsIGNyZWF0ZSBhbmQgbWFuaXB1bGF0ZSB0aGUgJ3RvZG8nIGxpc3Qgb2JqZWN0cy5cbiovXG5cbi8vIFRhc2sgZmFjdG9yeVxuXG5jb25zdCBUYXNrID0gKHRpdGxlLCBkZXNjcmlwdGlvbiwgZHVlRGF0ZSwgcHJpb3JpdHksIG5vdGVzLCBsb2NhdGlvbikgPT4ge1xuICByZXR1cm4ge1xuICAgIHRpdGxlLFxuICAgIGRlc2NyaXB0aW9uLFxuICAgIGR1ZURhdGUsXG4gICAgcHJpb3JpdHksXG4gICAgbm90ZXMsXG4gICAgbG9jYXRpb24sXG4gIH07XG59O1xuXG5jb25zdCB0YXNrQ29udHJvbGxlciA9ICgoKSA9PiB7XG4gIGNvbnN0IHRhc2tzID0gW107XG4gIFxuICBjb25zdCBjcmVhdGVUYXNrcyA9ICgpID0+IHtcblxuICB9XG5cbn0pXG5cblxuLy9cblxuXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
